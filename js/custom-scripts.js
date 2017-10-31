874ab7bc6b2e08ae4b3a08668bab80ad6259d197
Iegor_Stetsiura <biropka@gmail.com>
2017-10-31 15:07:40 +0200

    v 0.1

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -15,4 +15,50 @@ $(window).ready(function() {
 
     /* end of header menu */
 
+    /* slider banner */
+
+    $('.banner-main').slick({
+        arrows: false,
+        dots: true
+    });
+
+    /* end of slider banner */
+
+    /* slider products */
+
+    $('.products-slider').slick({
+        slidesToShow: 4,
+        appendArrows: '.products-control',
+        prevArrow: '.products-control-prev',
+        nextArrow: '.products-control-next',
+        responsive: [
+            {
+                breakpoint: 1301,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 481,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+
+    /* end of slider products */
+
 });
