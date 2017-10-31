0ec65df3525f2c335609bfd3f05390f3f385a241
Iegor_Stetsiura <biropka@gmail.com>
2017-10-31 02:19:09 +0200

    v 0.1

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -4,6 +4,15 @@ $(window).ready(function() {
         $('.wrapper').animate({opacity: 1}, 500);
     }, 500);
 
+    /* header menu */
 
+    $('.header-submenu').hover(function() {
+        if($(window).width() > 1199) {
+            $(this).toggleClass('active');
+        }
+
+    });
+
+    /* end of header menu */
 
 });
