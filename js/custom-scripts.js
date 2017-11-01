b91cb575afcfef758a89f5587ac1368aa11bdd34
Iegor_Stetsiura <biropka@gmail.com>
2017-11-01 03:19:32 +0200

    v 0.1

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -4,6 +4,61 @@ $(window).ready(function() {
         $('.wrapper').animate({opacity: 1}, 500);
     }, 500);
 
+    if($(window).scrollTop() > 0) {
+        $('.header-top').addClass('shortest');
+    } else {
+        $('.header-top').removeClass('shortest');
+    }
+
+    $(window).scroll(function() {
+
+        if($(window).scrollTop() > 0) {
+            $('.header-top').addClass('shortest');
+        } else {
+            $('.header-top').removeClass('shortest');
+        }
+
+    });
+
+    if($(window).width() < 1200) {
+        $('.header-sign').prependTo('.mobile-nav');
+        $('.header-local').prependTo('.mobile-nav');
+    } else {
+        $('.header-local').insertBefore('.header-logo');
+        $('.header-sign').insertAfter('.burger');
+    }
+
+    if($(window).width() < 992) {
+        $('.header-menu').appendTo('.mobile-nav');
+    } else {
+        $('.header-menu').insertBefore('.burger');
+    }
+
+    $(window).resize(function() {
+
+        if($(window).width() < 1200) {
+            $('.header-sign').prependTo('.mobile-nav');
+            $('.header-local').prependTo('.mobile-nav');
+        } else {
+            $('.header-local').insertBefore('.header-logo');
+            $('.header-sign').insertAfter('.burger');
+        }
+
+        if($(window).width() < 992) {
+            $('.header-menu').appendTo('.mobile-nav');
+        } else {
+            $('.header-menu').insertBefore('.burger');
+        }
+
+    });
+
+    $('.burger').click(function() {
+
+        $(this).toggleClass('active');
+        $('.mobile-nav').toggleClass('active');
+
+    });
+
     /* header menu */
 
     $('.header-submenu').hover(function() {
@@ -13,7 +68,23 @@ $(window).ready(function() {
 
     });
 
-    /* end of header menu */
+    $('.header-submenu-label').click(function() {
+        if ($(window).width() > 991 && $(window).width() < 1200) {
+            $(this).parent().toggleClass('active');
+        }
+
+    });
+
+    $(document).click(function(event) {
+
+        if (!$(event.target).closest(".header-submenu").length) {
+            $('.header-submenu').removeClass('active');
+        }
+
+    });
+
+
+        /* end of header menu */
 
     /* slider banner */
 
